ecc21b771e7209864f04e4d0550ee44880966043
Zachary Murphy <murphyz@my.wccc.edu>
2016-11-01 20:51:33 -0400

    initial

---
+++ ch15_ex2/model/validate.php
@@ -0,0 +1,79 @@
+<?php
+class Validate {
+    private $fields;
+
+    public function __construct() {
+        $this->fields = new Fields();
+    }
+
+    public function getFields() {
+        return $this->fields;
+    }
+
+    // Validate a generic text field
+    public function text($name, $value,
+            $required = true, $min = 1, $max = 255) {
+
+        // Get Field object
+        $field = $this->fields->getField($name);
+
+        // If field is not required and empty, remove errors and exit
+        if (!$required && empty($value)) {
+            $field->clearErrorMessage();
+            return;
+        }
+
+        // Check field and set or clear error message
+        if ($required && empty($value)) {
+            $field->setErrorMessage('Required.');
+        } else if (strlen($value) < $min) {
+            $field->setErrorMessage('Too short.');
+        } else if (strlen($value) > $max) {
+            $field->setErrorMessage('Too long.');
+        } else {
+            $field->clearErrorMessage();
+        }
+    }
+
+    // valid numeric entities check #6
+    public function number($name, $value, $required = true) {
+
+        $field = $this->fields->getField($name);
+
+        $this->text($name, $value, $required);
+        if ($field->hasError()) { return; }
+
+        if (!is_numeric($value)) {
+            $field->setErrorMessage('Must be a valid number.');
+        } else {
+            $field->clearErrorMessage();
+        }
+    }
+
+    // Validate a field with a generic pattern
+    public function pattern($name, $value, $pattern, $message,
+            $required = true) {
+
+        // Get Field object
+        $field = $this->fields->getField($name);
+
+        // If field is not required and empty, remove errors and exit
+        if (!$required && empty($value)) {
+            $field->clearErrorMessage();
+            return;
+        }
+
+        // Check field and set or clear error message
+        $match = preg_match($pattern, $value);
+        if ($match === false) {
+            $field->setErrorMessage('Error testing field.');
+        } else if ( $match != 1 ) {
+            $field->setErrorMessage($message);
+        } else {
+            $field->clearErrorMessage();
+        }
+    }
+
+   
+}
+?>
